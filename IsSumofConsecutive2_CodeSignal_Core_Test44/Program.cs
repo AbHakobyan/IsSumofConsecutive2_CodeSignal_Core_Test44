f49e2665b9ba8637bb1d35f3b269d9ffa72a8d80
Albert Hakobyan <abhakobyan24@gmail.com>
2021-09-18 10:22:47 +0400

    Добавьте файлы проекта.

---
+++ IsSumofConsecutive2_CodeSignal_Core_Test44/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsSumofConsecutive2_CodeSignal_Core_Test44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 8;
+            int x = isSumOfConsecutive2(n);
+            Console.WriteLine(x);
+            
+        }
+
+        static int isSumOfConsecutive2(int n)
+        {
+            int count = 0;
+            for (int i = 1; i < n; i++)
+            {
+                int sum = i, j = 1;
+                while (sum < n)
+                {
+                    sum = sum + i + j;
+                    j++;
+                }
+                if (sum == n) count++;
+            }
+            return count;
+        }
+
+    }
+}
